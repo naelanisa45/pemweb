435792da5bc01286b54a5eff12bb3b043214fc25
naelanisa45 <naelanisa45@gmail.com>
2026-05-22 22:11:51 +0700

    uts

--- be-web2/src/index.ts
+++ be-web2/src/index.ts
@@ -5,7 +5,7 @@ import categoryRoutes from "./routes/categoryRoute.js";
 import pembicaraRoutes from "./routes/pembicaraRoute.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
